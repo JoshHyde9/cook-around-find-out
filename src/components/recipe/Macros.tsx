827dc4060064ec42462dbc7ab663a8109d007b54
JoshHyde9 <joshhyde546@gmail.com>
2024-03-16 10:50:26 +1100

    feat: added basic components to recipe page

---
+++ src/components/recipe/Macros.tsx
@@ -0,0 +1,20 @@
+import { component$ } from "@builder.io/qwik";
+
+type MacrosProps = {
+  macros: [{ title: string; value: number }];
+};
+
+export const Macros = component$<MacrosProps>(({ macros }) => {
+  return (
+    <div class="flex justify-between">
+      {macros.map(({ title, value }, i) => {
+        return (
+          <div key={i} class="flex flex-col items-center">
+            <h2 class="text-3xl font-bold">{title}</h2>
+            <p class="text-xl">{value}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+});
