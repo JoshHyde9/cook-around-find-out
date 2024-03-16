53f13d0c6c0425bb596e855257bff51f0d03f17c
JoshHyde9 <joshhyde546@gmail.com>
2024-03-17 08:26:55 +1100

    fix: styling & formatting

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -19,12 +19,18 @@ export const Switch = component$<SwitchProps>(({ items }) => {
 
   return (
     <div>
-      <div class="flex border-b border-gray-200 dark:border-gray-700 dark:text-gray-400">
+      <div class="flex justify-around">
         {items.map(({ title }, i) => {
           return (
             <button
               key={i}
-              class={`mx-4 ${state.value === title ? "inline-block rounded-t-lg bg-gray-100 p-4 text-blue-600 dark:bg-gray-800 dark:text-blue-500" : "inline-block rounded-t-lg p-4 hover:bg-gray-50 hover:text-gray-600 dark:hover:bg-gray-800 dark:hover:text-gray-300"}`}
+              class={`inline-block w-full rounded-t-lg p-4 
+              ${
+                state.value === title
+                  ? "border-b-2 border-blue-600 text-blue-600 hover:text-blue-600 dark:text-blue-600"
+                  : "border-b-2 hover:border-blue-600 hover:text-blue-600 dark:hover:text-blue-600"
+              }
+                `}
               onClick$={() => (state.value = title)}
             >
               {title}
