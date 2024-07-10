53ae7a2c3a2937efc5c2fd04a876c797efcb105b
JoshHyde9 <joshhyde546@gmail.com>
2024-07-10 18:27:54 +1000

    render recipe cards

---
+++ src/components/Card.tsx
@@ -0,0 +1,47 @@
+import type { RecipeCollectionEntry } from "~/content";
+import { component$ } from "@builder.io/qwik";
+import { useFormatDate } from "qwik-speak";
+
+export const Card = component$<RecipeCollectionEntry>(({ data, slug }) => {
+  const fd = useFormatDate();
+
+  return (
+    <a href={slug}>
+      <div class="flex flex-col overflow-hidden rounded shadow-lg">
+        <div class="relative">
+          <img
+            class="w-full"
+            src={data.thumbnail.src}
+            alt={data.thumbnail.alt}
+            width={0}
+            height={0}
+          />
+          <div class="absolute bottom-0 left-0 right-0 top-0 bg-gray-900 opacity-25 transition duration-300 hover:bg-transparent"></div>
+        </div>
+        <div class="mb-auto px-6 py-4">
+          <div>
+            <h2>{data.title}</h2>
+          </div>
+          <p class="text-sm text-gray-500">{data.description}</p>
+        </div>
+        <div class="flex flex-row items-center justify-between bg-gray-100 px-6 py-3">
+          <span class="font-regular mr-1 flex flex-row items-center py-1 text-xs text-gray-900">
+            {fd(data.date, {
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            })}
+          </span>
+
+          <div class="font-regular mr-1 flex flex-row items-center py-1 text-xs text-gray-900">
+            {data.tags?.map((tag) => (
+              <span key={tag} class="rounded-sm bg-slate-200 px-3 py-1">
+                {tag}
+              </span>
+            ))}
+          </div>
+        </div>
+      </div>
+    </a>
+  );
+});
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,23 +1,31 @@
 import { component$ } from "@builder.io/qwik";
-import type {
-  DocumentHead,
-  StaticGenerateHandler,
+import {
+  type DocumentHead,
+  type StaticGenerateHandler,
 } from "@builder.io/qwik-city";
 import { inlineTranslate } from "qwik-speak";
+import { Card } from "~/components/Card";
+import { useRecipes } from "~/content";
 
 import { config } from "~/speak-config";
 
 export default component$(() => {
   const t = inlineTranslate();
+
+  const recipes = useRecipes();
+
   return (
-    <>
-      <h1>Hi 👋</h1>
-      <div>
-        Can't wait to see what you build with qwik!
-        <br />
-        {t("site.messages.why?")}
+    <div class="container mx-auto px-2 md:px-0">
+      <div class="my-10 text-center">
+        <h1 class="text-4xl">{t("site.home.title")}</h1>
+        <p class="text-neutral-500">{t("site.home.description")}</p>
       </div>
-    </>
+      <div class="grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3">
+        {recipes.value.map((recipe) => (
+          <Card key={recipe.slug} slug={recipe.slug} data={recipe.data} />
+        ))}
+      </div>
+    </div>
   );
 });
 
@@ -40,3 +48,5 @@ export const onStaticGenerate: StaticGenerateHandler = () => {
     }),
   };
 };
+
+export { useRecipes } from "~/content";
