c1ed414d0e99be6190e84ea2240ad3dbdbb5ca11
JoshHyde9 <joshhyde546@gmail.com>
2024-07-25 11:09:30 +1000

    I don't know how to fix this

--- src/routes/plugin.ts
+++ src/routes/plugin.ts
@@ -1,22 +1,21 @@
 import type { RequestHandler } from "@builder.io/qwik-city";
-
-import { extractFromUrl, setSpeakContext, validateLocale } from "qwik-speak";
+import { setSpeakContext, validateLocale } from "qwik-speak";
 
 import { config } from "../speak-config";
 
 /**
  * This middleware function must only contain the logic to set the locale,
- * because it is invoked on every request to the server. Avoid redirecting or
- * throwing errors here, and prefer layouts or pages
+ * because it is invoked on every request to the server.
+ * Avoid redirecting or throwing errors here, and prefer layouts or pages
  */
-export const onRequest: RequestHandler = ({ locale, url }) => {
-  let lang: string | undefined;
+export const onRequest: RequestHandler = ({ params, locale }) => {
+  let lang: string | undefined = undefined;
 
-  const prefix = extractFromUrl(url);
-
-  if (prefix && validateLocale(prefix)) {
+  if (params.lang && validateLocale(params.lang)) {
     // Check supported locales
-    lang = config.supportedLocales.find((value) => value.lang === prefix)?.lang;
+    lang = config.supportedLocales.find(
+      (value) => value.lang === params.lang,
+    )?.lang;
   } else {
     lang = config.defaultLocale.lang;
   }
